bc349fd138ed82c8ce9a06d6fda1bdc5bd377572
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-16 18:20:33 -0300

    update

--- android.sh
+++ android.sh
@@ -3,13 +3,15 @@ set -e
 
 QT_VERSION=6.5.3
 JENV_VERSION=openjdk64-17.0.16
+NDK=26.2.11394342
+android-platform-manager.sh profile-load  Qt-QML-CuteMobile
 
 # === Qt ${QT_VERSION} (ajuste se o caminho for outro) ===
 export QT_BASE_DIR="${HOME}/qt"
 export QT_HOST_DIR="${QT_BASE_DIR}/${QT_VERSION}/gcc_64"
 
-QT_ANDROID_ARM64_DIR="${QT_BASE_DIR}/6.5.3/android_arm64_v8a"
-QT_ANDROID_ARMV7_DIR="${QT_BASE_DIR}/6.5.3/android_armv7"
+QT_ANDROID_ARM64_DIR="${QT_BASE_DIR}/${QT_VERSION}/android_arm64_v8a"
+QT_ANDROID_ARMV7_DIR="${QT_BASE_DIR}/${QT_VERSION}/android_armv7"
 
 #export QT_ANDROID_DIR="${QT_ANDROID_ARMV7_DIR}"
 export QT_ANDROID_DIR="${QT_ANDROID_ARM64_DIR}"
@@ -17,7 +19,7 @@ export QT_ANDROID_DIR="${QT_ANDROID_ARM64_DIR}"
 # === Android SDK/NDK ===
 export ANDROID_SDK_ROOT="${HOME}/Android/Sdk"
 export ANDROID_HOME="${ANDROID_SDK_ROOT}"
-export ANDROID_NDK_ROOT="${ANDROID_SDK_ROOT}/ndk/26.2.11394342"
+export ANDROID_NDK_ROOT="${ANDROID_SDK_ROOT}/ndk/${NDK}"
 
 # === Java (via jenv, como você já usa) ===
 export JENV_VERSION=${JENV_VERSION}
@@ -39,6 +41,18 @@ qt-cmake .. \
     -DQT_HOST_PATH="${QT_HOST_PATH}" \
     -DANDROID_ABI=arm64-v8a \
     -DANDROID_PLATFORM=android-33
+    # -DQT_ANDROID_COMPILE_SDK_VERSION=33 \
+    # -DQT_ANDROID_MIN_SDK_VERSION=21 \
+    # -DQT_ANDROID_TARGET_SDK_VERSION=33
+
+# CMake Warning:
+#   Manually-specified variables were not used by the project:
+#
+#     QT_ANDROID_COMPILE_SDK_VERSION
+#     QT_ANDROID_MIN_SDK_VERSION
+#     QT_ANDROID_TARGET_SDK_VERSION
+
+
 
 # Compila
 cmake --build .
